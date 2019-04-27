9be28314f6268d17e69a1406e04e53b6bfd4454d
mojun-ck <mojun-ck@aliyun.com>
2019-04-27 22:27:15 +0800

    编程式路由导航

--- vue_blank/src/views/Message.vue
+++ vue_blank/src/views/Message.vue
@@ -3,8 +3,14 @@
     <ul>
       <li v-for="(message,index) in messages" :key="message.id">
         <router-link :to="`/home/message/detail/${message.id}`">{{message.title}}</router-link>
+        <button @click="pushShow(message.id)">push查看</button>
+        <button @click="replaceShow(message.id)">replace查看</button>
       </li>
     </ul>
+    <hr>
+    <button @click="$router.back()">回退</button>
+    <hr>
+
     <router-view></router-view>
   </div>
 
@@ -32,6 +38,14 @@
         ]
         this.messages = messages
       },1000)
+    },
+    methods:{
+      pushShow(id){
+        this.$router.push(`/home/message/detail/${id}`)
+      },
+      replaceShow(id){
+        this.$router.replace(`/home/message/detail/${id}`)
+      }
     }
   }
 </script>
